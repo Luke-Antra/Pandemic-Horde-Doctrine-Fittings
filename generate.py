c78621f367c24d97b1b7ecf01f32c6eb23d10e8c
Natalie Frost <frostbite@frosty-nee.net>
2020-05-28 02:04:01 -0400

    added forum post generator
    will take the template.txt file from a doctrine directory and replace fit placeholders
    Placeholder is [Fit=File-Name] without the file extension
    
    Call the generator with ./generator.py Ferox

---
+++ generate.py
@@ -0,0 +1,32 @@
+#!python
+
+import os
+import sys
+
+if __name__ == '__main__': 
+    if len(sys.argv) < 2: 
+        print('Please specify a directory to generate a post for')
+    if len(sys.argv) == 2:
+        if sys.argv[1][-1] != '/': sys.argv[1]+='/' #add trailing / to input if it's not already there
+        os.chdir(sys.argv[1])
+        try:
+            with open('template.txt', 'r') as template:
+                template= template.readlines()
+        except Exception as e:
+            print(e)
+        while True:
+            for line in range(len(template)):
+                if '[Fit=' in template[line]:
+                    print('inserting fit {}'.format(template[line]))
+                    fit_file = template[line][5:-2].rstrip()
+                    template.pop(line)
+                    with open(fit_file+'.txt', 'r') as fit:
+                        fit = fit.readlines()
+                        fit.reverse()
+                        for mod in fit:
+                            template.insert(line, mod)
+                    break
+                if line == len(template)-1:
+                    with open('out.txt', 'w') as output:
+                        output.writelines(template)
+                    sys.exit()
